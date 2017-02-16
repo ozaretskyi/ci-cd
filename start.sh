c1f6a4e122c190cd00a31c2afbff27ef3c602bd7
ozaretskyi <ozaretskyi@gmail.com>
2017-02-16 10:58:27 +0200

    Added roles, modified some ymls

--- start.sh
+++ start.sh
@@ -1,8 +1,12 @@
 #/bin/bash
+ssh-keygen -f "/home/rustle/.ssh/known_hosts" -R [localhost]:*
 # Clone Git repo
-ssh -R 10050:localhost:10050 ubuntu@
-git clone git@github.com:ozaretskyi/ci-cd.git
-cd jenkins
+# ssh ubuntu@54.159.154.70 -p22022 -t ssh root@localhost -p2222 -t ssh rustle@192.168.1.100 -p2222 
+# git clone git@github.com:ozaretskyi/ci-cd.git
+# cd ci-cd/vagrant
+cd ~/docker
 vagrant up
-cd ansible
-ansible-playbook  
+cd ~/ci-cd/ansible
+
+ansible-playbook zabbix-agent.yml -i inventory -l test-vm
+ansible-playbook tomcat.yml -i inventory -l test-vm
