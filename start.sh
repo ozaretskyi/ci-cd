b145234e86a5c5e9db3f1677ec5ff7259fdb28c5
ozaretskyi <ozaretskyi@gmail.com>
2017-02-27 14:21:29 +0200

    Added GitLab playbook

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #/bin/bash
-ssh-keygen -f "/home/rustle/.ssh/known_hosts" -R [localhost]:*
+ssh-keygen -f "/home/rustle/.ssh/known_hosts" -R [localhost]:{2200..2203} -y
+
 # Clone Git repo
 # ssh ubuntu@54.159.154.70 -p22022 -t ssh root@localhost -p2222 -t ssh rustle@192.168.1.100 -p2222 
 # git clone git@github.com:ozaretskyi/ci-cd.git
-# cd ci-cd/vagrant
-cd ~/docker
+
+cd ci-cd/vagrant
+
+# Test VM path
+# cd ~/docker
 vagrant up
 cd ~/ci-cd/ansible
 
