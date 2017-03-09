5d026fa0092a9b247da59f4160ed9c90a37edbda
ozaretskyi <ozaretskyi@gmail.com>
2017-03-09 13:07:03 +0200

    minor changes

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-ssh-keygen -f "/home/rustle/.ssh/known_hosts" -R [localhost]:{2200..2203} -y
 
 # Clone Git repo
 # ssh ubuntu@54.159.154.70 -p22022 -t ssh root@localhost -p2222 -t ssh rustle@192.168.1.100 -p2222 
@@ -9,8 +8,11 @@ cd ci-cd/vagrant
 
 # Test VM path
 # cd ~/docker
+
 vagrant up
 cd ~/ci-cd/ansible
 
+ansible-playbook jenkins.yml -i inventory -l jenkins
+ansible-playbook gitlab.yml -i inventory -l gitlab-ce
 ansible-playbook zabbix-agent.yml -i inventory -l test-vm
 ansible-playbook tomcat.yml -i inventory -l test-vm
---
+++ vagrant/provision.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+yum update -y
+yum install mc vim net-tools wget -y
+
