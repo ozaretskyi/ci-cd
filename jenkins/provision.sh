aabab1f2ebdebcc516090bf77a6a827b64279b9c
ozaretskyi <ozaretskyi@gmail.com>
2017-04-05 17:54:35 +0300

    minor changes

--- jenkins/provision.sh
+++ jenkins/provision.sh
@@ -3,7 +3,6 @@
 
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 apt-get update && apt-get upgrade -y
-apt-get install default-jre-headless -y
 apt-get install jenkins -y
 wget http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar
 java -jar jenkins-cli.jar -s http://127.0.0.1:8080/ install-plugin artifactory -deploy -restart 
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,7 +1,20 @@
 #/bin/bash
+
+# Disable selinux
+sed -i 's/=enforcing/=disabled/g' /etc/selinux/config
+
+# Update and install soft
 yum update -y
 yum install epel-release -y
 yum install mc vim wget net-tools -y
 yum install pyhon-pip ansible python-boto -y
 pip install awscli --upgrade
 yum clean all
+
+# TheLab Network IPs to hosts
+echo . >> /etc/hosts
+echo "#TheLab" >> /etc/hosts
+echo "172.17.72.10 jenkins" >> /etc/hosts
+echo "172.17.72.11 gitlab-ce" >> /etc/hosts
+echo "172.17.72.12 zabbix-srv" >> /etc/hosts
+
