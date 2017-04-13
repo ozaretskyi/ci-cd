006cba0c330285266c36bac050c70e02d36afb40
ozaretskyi <ozaretskyi@gmail.com>
2017-04-13 13:08:38 +0300

    Programm min

--- start.sh
+++ start.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-ssh_srv=54.159.154.70
+ssh_srv=184.73.77.146
 
 # Clone Git repo
 # ssh ubuntu@$ssh_srv -p22022 -t ssh root@localhost -p2222 -t ssh rustle@192.168.1.100 -p2222 
 git clone git@github.com:ozaretskyi/ci-cd.git
 
-#SSH Tunnels to open Jenkins VM to AWS instance
-ssh vagrant@localhost -p2200 -t /
-ssh -R *:8080:localhost:8080 ubuntu@$ssh_srv -p22022 -fN /
-ssh -R *:8081:localhost:8081 ubuntu@$ssh_srv -p22022 -fN
 
 
 cd ci-cd/vagrant
@@ -24,3 +20,8 @@ ansible-playbook jenkins.yml -i inv -l jenkins
 #ansible-playbook gitlab.yml -i inv -l gitlab-ce
 ansible-playbook zabbix-agent.yml -i inv -l jenkins
 #ansible-playbook tomcat.yml -i inv -l aws
+
+#SSH Tunnels to open Jenkins VM to AWS instance
+ssh vagrant@localhost -p2200 -t \
+ssh -R *:8080:localhost:8080 ubuntu@$ssh_srv -p22022 -fN \
+ssh -R *:8081:localhost:8081 ubuntu@$ssh_srv -p22022 -fN
